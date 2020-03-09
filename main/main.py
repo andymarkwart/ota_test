bcb7637f235eea0fc356437ca5f9390a8c87c524
andy.markwart <andy.markwart@gmail.com>
2020-03-10 00:10:29 +0100

    version 0.4

--- main/main.py
+++ main/main.py
@@ -2,7 +2,7 @@ import credentials as c
 import sys
 from machine import Pin
 
-from ota_updater import OTAUpdater
+from main.ota_updater import OTAUpdater
 o = OTAUpdater('https://github.com/andymarkwart/ota_test')
 
 btn0 = Pin(0, Pin.IN, Pin.PULL_UP)
@@ -13,6 +13,7 @@ def download_and_install_update_if_available(ssid, psk):
     o.download_and_install_update_if_available(ssid, psk)
 
 def check_for_updates(ssid, psk):
+    print("Checking for updates...")
     o.using_network(ssid, psk)
     o.check_for_update_to_install_during_next_reboot()
     
@@ -21,7 +22,7 @@ def start():
     # from main.x import project
     # project = YourProject()
     # ...
-    print("\n>>>\nProgram MAIN\nVersion: 0.3\n<<<\n")
+    print("\n>>>\nProgram MAIN\nVersion: 0.4\n<<<\n")
 
 
 def boot():
@@ -29,7 +30,7 @@ def boot():
         print("Button 0 low, exiting...")
         sys.exit()
     elif btn1.value() == 0:
-        check_for_updates(c.ssid, c.psk):
+        check_for_updates(c.ssid, c.psk)
     else:
         download_and_install_update_if_available(c.ssid, c.psk)
         start()
