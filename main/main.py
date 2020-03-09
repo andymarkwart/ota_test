56cb8a9060f90ab5f5fdfbf3e7fa0a0f616940e6
andy.markwart <andy.markwart@gmail.com>
2020-03-09 23:54:00 +0100

    version 0.3

--- main/main.py
+++ main/main.py
@@ -3,29 +3,35 @@ import sys
 from machine import Pin
 
 from ota_updater import OTAUpdater
+o = OTAUpdater('https://github.com/andymarkwart/ota_test')
 
 btn0 = Pin(0, Pin.IN, Pin.PULL_UP)
+btn1 = Pin(35, Pin.IN, Pin.PULL_UP)
 
-def download_and_install_update_if_available():
-    o = OTAUpdater('https://github.com/andymarkwart/ota_test')
-    #o.using_network(c.ssid, c.psk)
-    #o.check_for_update_to_install_during_next_reboot()
-    o.download_and_install_update_if_available("OnePlus5", "andyandy)
 
+def download_and_install_update_if_available(ssid, psk):
+    o.download_and_install_update_if_available(ssid, psk)
+
+def check_for_updates(ssid, psk):
+    o.using_network(ssid, psk)
+    o.check_for_update_to_install_during_next_reboot()
+    
 
 def start():
     # from main.x import project
     # project = YourProject()
     # ...
-    print("\n>>>\nProgram MAIN\nVersion: 0.2\n<<<\n")
+    print("\n>>>\nProgram MAIN\nVersion: 0.3\n<<<\n")
 
 
 def boot():
     if btn0.value() == 0:
         print("Button 0 low, exiting...")
         sys.exit()
+    elif btn1.value() == 0:
+        check_for_updates(c.ssid, c.psk):
     else:
-        download_and_install_update_if_available()
+        download_and_install_update_if_available(c.ssid, c.psk)
         start()
 
 
